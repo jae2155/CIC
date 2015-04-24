04b2fc67a975c3d772a391d91cfe77eb39a70b0b
Adam Reis <adamhreis@gmail.com>
2015-04-24 01:48:44 -0400

    prettified the code a bit

--- p3.py
+++ p3.py
@@ -1,21 +1,42 @@
-from election import est_percentages
+#######################
+# Jane Doe
+# jd1234
+# Computing in Context Spring '15, Assignment 5
+# This script estimates the number of ways there could have been a tie in the
+# 2000 presidential elections given some prior information about which states
+# were in the bag for each party and which were battleground states.
+#######################
 
-trials = 1000000
+from election import est_probs
 
-b_ground_votes = [6,3,25,22,7,4,18,10,11,4,4,7,23,11,11,5,11]
-b_ground_probs_actual = [.395,.143,.893,.999,.42,.5,.997,.999,.236,.146,.731,.602,.989,.289,.753,.302,.946]
-b_ground_probs_naive = [.5 for k in range(len(b_ground_votes))]
+def main():
+    battleground_votes = [6, 3, 25, 22, 7, 4, 18, 10, 11, 4, 4, 7, 23, 11, 11, 5, 11]
+    battleground_probs_naive = [.5]*len(battleground_votes)
+    battleground_probs_actual = [
+        .395, .143, .893, .999,  .42,   .5, .997, .999, .236, 
+        .146, .731, .602, .989, .289, .753, .302, .946
+    ]
+    
+    gore_votes = 146
+    trials = 1000000
 
-gore = 146
+    def print_simulation_results(probs):
+        """
+        Calculates and prints results of simulating election with given probs
 
-dem,ties,rep = est_percentages(b_ground_votes,trials,b_ground_probs_naive,gore)
-print("3.a")
-print("Probability Gore Wins: ",dem)
-print("Probability of Tie: ",ties)
-print("Probability Bush Wins: ",rep)
+        Parameters:
+            probs - a list of probabilities that the democrats will win each state
+        """
+        dem, tie, rep = est_probs(battleground_votes, trials, probs, gore_votes)
+        
+        print("\tProbability Gore Wins:\t", dem)
+        print("\tProbability of Tie:   \t", tie)
+        print("\tProbability Bush Wins:\t", rep)    
+    
+    print("3.a -- naive probabilities")
+    print_simulation_results(battleground_probs_naive)
 
-dem,ties,rep = est_percentages(b_ground_votes,trials,b_ground_probs_actual,gore)
-print("3.b")
-print("Probability Gore Wins: ",dem)
-print("Probability of Tie: ",ties)
-print("Probability Bush Wins: ",rep)
+    print("3.b -- polled probabilities")
+    print_simulation_results(battleground_probs_actual)    
+
+main()
